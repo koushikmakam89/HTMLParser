7a6ebf3124342d423939cfc337a13b44192cc33c
kmakam <kmakam@vmware.com>
2019-07-18 01:48:08 +0530

    Basic level working code for row-span

--- src/HTMLParser.py
+++ src/HTMLParser.py
@@ -1,5 +1,8 @@
 import lxml.html as lh
 from lxml import etree
+from collections import defaultdict
+import string
+import random
 
 class HTMLParser():
 
@@ -28,18 +31,202 @@ class HTMLParser():
         val = htmlValue.split(self.getIterater())[-1]
         return val.rstrip(self._templateSufix).lower()
 
-    def _replaceRowData(self,rowInformation,jsonArrayName,htmlElementsList,sortOrder = []):
-        def sort(json):
-            if len(sortOrder) > 0:
-                for item in sortOrder:
-                    newlist = sorted(json, key=lambda record: record[item], reverse=True)
-                return newlist
-            return json
+    """
+    This might be useful in the feature
+    """
+    def _groupBy(self,result,json,groupList,index,parent='',parentItem=''):
+        localParent = parent
+        localParentItem = parentItem
+        item = groupList[index]
+        for obj in json:
+            if localParent != '':
+                if obj[localParentItem] == localParent:
+                    newkey = obj[item]
+                    if obj[item] not in result[localParent]['child']:
+                        result[localParent]['child'][newkey] = {
+                            'records':[],
+                            'child':{},
+                            'selector':item
+                        }
+                    result[localParent]['child'][newkey]['records'].append(obj)
+            else:  
+                newkey = obj[item] 
+                if obj[item] not in result.keys():
+                    result[newkey] = {
+                        'records':[],
+                         'child':{},
+                         'selector':item
+                    }
+                result[newkey]['records'].append(obj)
 
+        index = index+1
+        if len(groupList) == index :
+            return result
+        else:
+            for key in result.keys():
+                self._groupBy(result,json,groupList,index,key,item)
+    
+    def _groupJson(self,jsonArrayName,groupList):
+        groups = {}
+        index = 0
+        def childIterator(groups,json,index,item):
+            for obj in json:
+                item = groupList[index]
+                key = obj[item]
+                if key not in groups.keys():
+                    groups[key] = {
+                        'child':{},
+                        'records':[]
+                    }
+                groups[key]['records'].append(obj)
+            return groups
+
+        if jsonArrayName in self.data:
+           
+            groups = childIterator(groups,self.data[jsonArrayName],index,groupList[index])
+            index = index+1 
+            for item in range(len(groupList)-1):
+                for key in groups.keys():
+                    obj = groups[key]
+                    record = groups[key]['records']
+                    if index > 1 :
+                        for i in range(index):
+                            if obj != None:
+                                for childKey in obj.keys():
+                                    if childKey not in ['records','child']:
+                                        obj[childKey]['child'] = childIterator(obj[childKey]['child'],obj['records'],index,groupList[index])
+                                if bool(obj) == True:
+                                    obj = obj.get('child')
+                                    record = obj.get('records')
+                    elif bool(obj) == True:
+                        obj = childIterator(obj,record,index,groupList[index])
+                index = index + 1 
+        return groups
+
+    def randomString(self,stringLength=5):
+        """Generate a random string of fixed length """
+        letters = string.ascii_lowercase
+        return ''.join(random.choice(letters) for i in range(stringLength))
+
+    def _generatRowSpanRows(self,groups,rowInformation,maxlevel,level,ignoreKeys,remainingRecords):
+        trElement = lh.fromstring(rowInformation).xpath('//tr')[0]
+        row = '<tr class="{}" style="{}">'.format(trElement.get('styles'),trElement.get('class'))+'{}</tr>{}'
+        result = ""
+        tdElements = (trElement).xpath('//td')
+        level = level + 1
+        
+        for key in groups.keys():
+            if key not in ignoreKeys:
+                rowData = ''
+                element = tdElements[level]
+                groupCount = 1
+                uniqurRecordCount = 0
+
+                uniqueRecords = defaultdict(int)
+                for item in groups[key]['records']:
+                    uniqueRecords[str(item)] += 1
+
+                if len(uniqueRecords) > 1:
+                    for _ in uniqueRecords.keys():
+                        uniqurRecordCount = uniqurRecordCount+1
+                
+                if groupCount > 1 :
+                    rowspan = groupCount+uniqurRecordCount - level
+                else:
+                    rowspan = groupCount+uniqurRecordCount 
+                
+                if uniqurRecordCount == len(groups[key]['records']):
+                    groupCount  = uniqurRecordCount
+                
+                if len(groups[key]['records'])>groupCount:
+                    groupCount = len(groups[key]['records'])
+
+                element.set('rowspan',str(rowspan))
+                element.text = key
+                rowData = rowData + etree.tostring(element).decode()
+                insertPoint = ''
+                uniqueKey = self.randomString()
+
+                #Add the remaining td data if row span is less then max row count 
+                if maxlevel != groupCount:
+                    insertPoint = '##{}##'.format(uniqueKey)
+                    item  = groups[key]['records'][0]
+                    trElement = lh.fromstring(rowInformation).xpath('//tr')[0]
+                    tdElements = (trElement).xpath('//td')
+                    for element in tdElements[(level+1):len(tdElements)]:
+                        htmlselector = element.text_content()
+                        htmlselector = htmlselector.replace(self._groupIdentifer,self._iterationIdentifer)
+                        element.text = item[self._getChildName(htmlselector)]
+                        rowData  = rowData + etree.tostring(element).decode()
+                
+                    listDetails  = []
+
+                    for rec in groups[key]['records']:
+                        if str(rec) != str(item):
+                            listDetails.append(rec)
+                    
+                    remainingRecords[uniqueKey] = {
+                        'level': level+1,
+                        'records':listDetails
+                    }
+                
+                
+                result = result + row.format(rowData,insertPoint)
+                nextLevel = False
+                for childKey in groups[key]:
+                    if childKey not in ignoreKeys: 
+                        nextLevel = True
+                        break
+                if nextLevel == True:
+                    result = result + self._generatRowSpanRows(groups[key],rowInformation,maxlevel,level,ignoreKeys,remainingRecords)
+        return result
+
+    def _addRemainingRows(self,rowInformation,html,jsonList):
+        for key in jsonList.keys():
+            selector = '##{}##'.format(key)
+            if html.find(selector) > -1:
+                trElement = lh.fromstring(rowInformation).xpath('//tr')[0]
+                row = '<tr class="{}" style="{}">'.format(trElement.get('styles'),trElement.get('class'))+'{}</tr>'
+                record = ''
+                for data in jsonList[key]['records']:
+                    trElement = lh.fromstring(rowInformation).xpath('//tr')[0]
+                    tdElements = (trElement).xpath('//td')
+                    rowData = ''
+                    level = jsonList[key]['level']
+                    for element in tdElements[level:len(tdElements)]:
+                        htmlselector = element.text_content()
+                        htmlselector = htmlselector.replace(self._groupIdentifer,self._iterationIdentifer)
+                        element.text = data[self._getChildName(htmlselector)]
+                        rowData  = rowData + etree.tostring(element).decode()
+                    record  = record + row.format(rowData)
+                html = html.replace(selector,record)
+
+        return html
+            
+    def _replaceGroupRowData(self,rowInformation,jsonArrayName,htmlElementsList,groupList):
+        records = ""
+        _ignoreKeys = ['records','child']
+        groups  =  self._groupJson(jsonArrayName,groupList)
+        
+        for key in groups.keys():
+            #__init__
+            remainingRecords = {}
+            spanRecords = ''
+            group = {}
+            group[key] = groups[key]
+            maxDataLength = 0
+           
+            maxDataLength = len(group[key]['records'])
+            spanRecords = self._generatRowSpanRows(group,rowInformation,maxDataLength,-1,_ignoreKeys,remainingRecords)
+            spanRecords = self._addRemainingRows(rowInformation,spanRecords,remainingRecords)
+            records = records + spanRecords
+
+        return records
+
+    def _replaceRowData(self,rowInformation,jsonArrayName,htmlElementsList):
         popupaledRow = ""
         if jsonArrayName in self.data:
             json = self.data[jsonArrayName]
-            json = sort(json)
             for data in json:
                 rowData = rowInformation
                 for element in htmlElementsList:
@@ -64,7 +251,6 @@ class HTMLParser():
         modifiedData = etree.tostring(doc).decode()
         table_elements = doc.xpath('//table')
         for t in table_elements:
-            #print(etree.tostring(t))
             table_element = lh.fromstring(etree.tostring(t).decode())
             tr_elements = table_element.xpath('//tr')
             for r in tr_elements:
@@ -77,39 +263,28 @@ class HTMLParser():
                     
                     # table flow records
                     td_data = []
-                    groupIndex=[]
                     sortOrder = []
 
-                    index = 0
                     for d in td_element:
                         content = d.text_content()
                         if content.find(self._groupIdentifer)> -1:
-                            groupIndex.append(index)
                             sortOrder.append(self._getChildName(
                                 content.replace(self._groupIdentifer,self._iterationIdentifer)))
                         td_data.append(content)
-                        index +=  1
-                    datarows = self._replaceRowData(thingToReplace,
+                    if len(sortOrder) > 0:
+                        datarows = self._replaceGroupRowData(thingToReplace,
                             self._getParentName(htmlValue = 
                                     next(sub for sub in td_data if sub)
                                     .replace(self._groupIdentifer,self._iterationIdentifer)),
                             td_data,sortOrder)
-                    
-                    #Code to add rowspans for grouped records
-                    if len(groupIndex) > 0:
-                        row_element = lh.fromstring(datarows).xpath('//tr')
-                        for index in groupIndex:
-                            td_element = lh.fromstring(etree.tostring(row_element[index]).decode()).xpath('//td')
-                            element = td_element[index]
-                            oldRecord = etree.tostring(element).decode()
-                            counter = datarows.count(element.text_content())
-                            td_element[index].set("rowspan",str(counter))
-                            newRecord = etree.tostring(element).decode()
-                            datarows = datarows.replace(oldRecord,newRecord,1)
-                            datarows = datarows.replace(oldRecord,'')
-
+                    else:
+                        datarows = self._replaceRowData(thingToReplace,
+                                self._getParentName(htmlValue = 
+                                        next(sub for sub in td_data if sub)
+                                        .replace(self._groupIdentifer,self._iterationIdentifer)),
+                                td_data)
                     modifiedData = modifiedData.replace(thingToReplace, datarows)
-    
+                    
         return modifiedData
 
     def _addRecordsToList(self, content, elementSelector):
